5c9d5d2fa918979046f52c1c0381ddfa36818d35
unknown <163233@ids.upchiapas.edu.mx>
2019-07-25 10:09:17 -0500

    test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,16 @@
-let request = require('supertest');
-let app = require('../index.js');
+const request = require('supertest');
+const app = require('../index.js');
+const assert = require('assert');
 
 describe('GET /test', ()=>{
-
- it('respuesta: URL Test', (done)=> {
+ it('Respuesta: URL Test', (done)=> {
  request(app).get('/test').expect('URL Test', done);
  });
+});
 
+
+describe('Suma de 2 + 2', function () {
+ it('La suma es 4', function () {
+        assert.equal(2+2, 4);
+    });
 });
